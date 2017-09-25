41f1c8f6f3700d0200610b1cf95adc0991056dec
Olivier Sasseville <olivier.sasseville@termlynx.com>
2017-09-25 16:33:40 -0400

    Added support of SpecVanish.

---
+++ Doc2web.Tests/Plugins/Style/Properties/SpecVanishCssPropertyTests.cs
@@ -0,0 +1,26 @@
+﻿using Doc2web.Plugins.Style;
+using Doc2web.Plugins.Style.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doc2web.Tests.Plugins.Style.Properties
+{
+    [TestClass]
+    public class SpecVanishCssPropertyTests
+    {
+        [TestMethod]
+        public void SetOn_Test()
+        {
+            var expected = new CssData();
+            expected.AddAttribute(".test", "display", "none");
+            var instance = new SpecVanishCssProperty { Selector = ".test" };
+
+            var cssData = new CssData();
+            instance.SetOn(cssData);
+
+            Assert.AreEqual(expected, cssData);
+        }
+    }
+}
---
+++ Doc2web/Plugins/Style/Properties/SpecVanishCssProperty.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doc2web.Plugins.Style.Properties
+{
+    public class SpecVanishCssProperty : BooleanCssProperty<SpecVanish>
+    {
+        public override void SetOff(CssData data) { }
+
+        public override void SetOn(CssData data)
+        {
+            data.AddAttribute(Selector, "display", "none");
+        }
+    }
+}
